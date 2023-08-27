b9618947c3f3edbc975433428f36b5ad2216fc17
kikuc <grimpoteuthistako@gmail.com>
2023-08-27 10:39:39 +0900

    VersionControlの依存関係を追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.media3:media3-exoplayer:1.1.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
